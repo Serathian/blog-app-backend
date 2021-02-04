488742e9db18aec2fcc565030422e6c958cf3b1e
Jake <jake.a.reddy@gmail.com>
2021-02-04 13:56:19 +0200

    extracted blog model, logger, env, config

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+const http = require('http')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
 
 app.use(cors())
 app.use(express.json())
