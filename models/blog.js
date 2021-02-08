84c6208dff05aeac139d75744407afb60f62add9
Jake <jake.a.reddy@gmail.com>
2021-02-08 11:42:03 +0200

    4.10 done, router post-async, toJSON and test implemented

--- models/blog.js
+++ models/blog.js
@@ -1,5 +1,6 @@
 const config = require('../utils/config')
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const mongoUrl = config.MONGODB_URI
 
 mongoose
@@ -18,5 +19,15 @@ const blogSchema = new mongoose.Schema({
   url: String,
   likes: Number,
 })
+
+blogSchema
+  .set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    },
+  })
+  .plugin(uniqueValidator)
 
 module.exports = mongoose.model('Blog', blogSchema)
